92bfe88b3454387d750a98bca0f13623219a3280
Luis Carlos Gomes <lgomes@braspag.com.br>
2018-08-10 22:02:01 -0300

    Criando filtro de exceção

---
+++ EmployeeManager.Api/Filters/ExceptionHandlerFilter.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Serilog;
+
+namespace EmployeeManager.Api.Filters
+{
+    public class ExceptionHandlerFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            if (context.ExceptionHandled == false)
+            {
+                Log.Error(context.Exception, "Error handled");
+            }
+        }
+    }
+}
