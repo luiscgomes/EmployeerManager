1c0df3913242b71ec22958abbe80bb199be1d119
Luis Carlos Gomes <lgomes@braspag.com.br>
2018-08-15 23:10:41 -0300

    Criando projeto de teste de integração

---
+++ EmployeeManager.IntegrationTests/DbContextOptionsFactory.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeManager.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace EmployeeManager.IntegrationTests
+{
+    internal static class DbContextOptionsFactory
+    {
+        internal static DbContextOptions Create(string databaseName = null) =>
+            new DbContextOptionsBuilder<EmployeeManagerContext>()
+                .UseInMemoryDatabase(databaseName: databaseName ?? Guid.NewGuid().ToString())
+                .Options;
+    }
+}
